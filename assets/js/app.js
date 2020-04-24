a412bb8bc0c5575c22b15dc17b09ca33898264ba
Folahanmi <emmanuelkolawole4@gmail.com>
2020-04-24 20:38:16 +0100

    setup the Products class with a getProducts() method that is then called on the class in the domcontentloaded event listener

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,13 +8,29 @@ const cartItems = document.querySelector('.cart-items');
 const cartTotal = document.querySelector('.cart-total');
 const cartContent = document.querySelector('.cart-content');
 const productsDOM = document.querySelector('.products-center');
+const productsLocalURL = `../../products.json`;
 
 // main cart... this is where i get and place information from the localStorage
 let cart = [];
 
 // getting the products
 class Products {
-
+    async getProducts() {
+        try {
+            let result = await fetch(productsLocalURL);
+            let data = await result.json();
+            let products = data.items;
+            products = products.map(item => {
+                const { title, price } = item.fields; 
+                const { id } = item.sys;
+                const image = item.fields.image.fields.file.url;
+                return { title, price, id, image };
+            });
+            return products;
+        } catch (e) {
+            console.log(e);
+        }
+    }
 }
 
 // display products
@@ -31,4 +47,7 @@ class Storage {
 document.addEventListener('DOMContentLoaded', () => {
     const ui = new UI();
     const products = new Products();
-})
+
+    // get all products
+    products.getProducts().then(products => console.table(products));
+});
